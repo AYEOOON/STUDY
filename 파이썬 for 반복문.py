e1c8c3529454ddf9f98a4b153b848853e316a20d
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-03-14 22:11:49 +0900

    Create 파이썬 for 반복문.py

---
+++ 파이썬 for 반복문.py
@@ -0,0 +1,20 @@
+for byunsoo in [10,20,30,40,50]:      # 10 20 30 40 50이 차례대로 출력됨
+  print(byunsoo)
+  
+  
+# for문을 횟수로 횟수문을 만들 때엔 range를 사용한다.
+  
+ for byunsoo in range(10):   # 부등호 같은 느낌
+    print(byunsoo)
+    
+ for i in range(10):
+    print(i)
+
+
+# 시작하는 숫자를 바꾸고 싶을 때
+ for i in range(3,10):          # 숫자 제일 앞에 시작하고 싶은 숫자를 적는다.
+    print(i)
+    
+# 시작하는 수 부터 2씩 증가시키고싶을 때
+ for i in range(3,10,2):        # 숫자 제일 뒤에 증가 시키고 싶은 수를 적는다.
+    print(i)
