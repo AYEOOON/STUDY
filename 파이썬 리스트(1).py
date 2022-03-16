da54087b4ae2ba8442bfc489e727c2b82e0b0fc1
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-03-16 19:07:15 +0900

    Update 파이썬 리스트(1).py

--- 파이썬 리스트(1).py
+++ 파이썬 리스트(1).py
@@ -12,3 +12,11 @@ a=input()
 c=eval(a)    #eval()이라는 함수는 함수자체 판단 하에 그 결과를 알려줌
 print(c)
 
+#3
+a=[10,20,30,40,50]
+print(a)
+print(a[0])
+print(a[1])        #[]안에 적힌 숫자는 리스트 안에서의 순서
+print(a[4])
+
+print(len(a))  
