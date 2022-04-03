eb1fcc2fb039fac14a1ad6610a9034457a7971c5
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-04-03 21:42:34 +0900

    Create 파이썬 round.py

---
+++ 파이썬 round.py
@@ -0,0 +1,15 @@
+# round 함수 예제
+
+print('round(0.5) : {0}'.format(round(0.5)))    # 0.5의 반올림이 1이 아니라 0이다.
+print('round(1.5) : {0}'.format(round(1.5)))
+print('round(2.5) : {0}'.format(round(2.5)))    # 2.5의 반올림이 3이 아니라 2이다. 
+print('round(3.5) : {0}'.format(round(3.5))) 
+print('round(4.5) : {0}'.format(round(4.5)))    # 4.5의 반올림이 5이 아니라 4이다. 
+print('round(5.5) : {0}'.format(round(5.5))) 
+print('round(6.5) : {0}'.format(round(6.5)))    # 6.5의 반올림이 7이 아니라 6이다.
+
+
+
+# 파이썬의 반올림은 반올림 하려는 수가 올림, 내림했을 때 동일하게 차이가 나는 경우에는 짝수 값으로 반올림 한다. 
+
+
